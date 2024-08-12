080f45abfc5b553270cfb15cc1f0f201547190bf
mike-droid <miguelreyesmoreno@hotmail.com>
2024-08-11 21:00:52 -0500

    linked list cycle

---
+++ linkedListCycle.ts
@@ -0,0 +1,21 @@
+import { ListNode } from "./oddEvenList";
+
+function hasCycle(head: ListNode | null): boolean {
+  if (!head) return false;
+
+  let slow = head;
+  let fast = head;
+
+  while (fast !== null && fast.next !== null) {
+    slow = slow.next!
+    fast = fast.next.next!
+    if (slow === fast) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const listNode: ListNode = new ListNode(3,	new ListNode(2,	new ListNode(0,	new ListNode(4))));
+const listNode2: ListNode = new ListNode(1,	new ListNode(2));
+const listNode3: ListNode = new ListNode(1);
